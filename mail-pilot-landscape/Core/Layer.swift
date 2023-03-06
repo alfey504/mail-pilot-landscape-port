6364d7f66a4b9530aa9ee15cd9b29baf1fbbe2c0
alfey504 <alfey504@gmail.com>
2023-03-05 22:40:27 -0500

    rename

--- mail-pilot-landscape/Core/Layer.swift
+++
@@ -1,6 +0,0 @@
-import GameplayKit
-
-enum Layer : CGFloat
-{
-    case ocean = 0, island = 1, plane = 2, cloud = 3
-}
--- mail-pilot-landscape/Managers/ScoreManager.swift
+++
@@ -1,8 +0,0 @@
-import SpriteKit
-import UIKit
-
-class ScoreManager
-{
-    public static var Score: Int = 0
-    public static var Lives: Int = 5
-}
